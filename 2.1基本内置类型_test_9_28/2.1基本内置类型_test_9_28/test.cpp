7d8e4bc4ec50e7d56452bd214dc7c43159302b04
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-09 18:30:52 +0800

    Add files via upload

---
+++ 2.1基本内置类型_test_9_28/2.1基本内置类型_test_9_28/test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+//int main()
+//{
+//	unsigned int u = 10;
+//	int i = -42;
+//
+//	std::cout << i + i << std::endl;
+//	std::cout << u + i << std::endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//for (unsigned int i = 10; i >= 0; --i)
+//	//	std::cout << i << std::endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	unsigned int u = 10, u2 = 42;
+//
+//	std::cout << u2 - u << std::endl;
+//	std::cout << u - u2 << std::endl;
+//
+//	int i = 10, i2 = 42;
+//
+//	std::cout << i2 - i << std::endl;
+//	std::cout << i - i2 << std::endl;
+//	std::cout << i - u<< std::endl;
+//	std::cout << u - i << std::endl;
+//
+//
+//	return 0;
+//}
+
+int main()
+{
+	std::cout << "\2\115" << "\n" << "\t" << "\115" << "\n" << std::endl;
+
+	return 0;
+}
