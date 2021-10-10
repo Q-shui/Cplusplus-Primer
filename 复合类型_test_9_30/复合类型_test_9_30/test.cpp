6e3ef4aea25344793d1d492c70d249c6973e7e05
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-10 23:40:53 +0800

    Add files via upload

---
+++ 复合类型_test_9_30/复合类型_test_9_30/test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+//引用
+
+//int main()
+//{
+//	int ival = 2;
+//	int &reival = ival;
+//	
+//	ival = 3;
+//
+//	std::cout << reival << std::endl;//对ival进行改动也是对reival进行改动
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0, & r1 = i;
+//	double d = 0, & r2 = d;
+//
+//	r2 = 3.14;
+//	r2 = r1;
+//	i = r2;
+//	r1 = d;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, & r1 = i;
+//	
+//	i = 5, r1 = 10;
+//
+//	std::cout << i << " " << r1 << std::endl;
+//
+//	return 0;
+//}
+
+//指针
+
+//int main()
+//{
+//	int i = 0, * pi = &i;
+//	int j = 1, * pj = &j;
+//
+//	pi = pj;
+//
+//	*pi = 0;
+//
+//	std::cout << i << j << std::endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 42;
+//	int* pi = &i;
+//
+//	*pi = *pi * *pi;
+//
+//	std::cout << i << std::endl;
+//
+//	return 0;
+//}
+
