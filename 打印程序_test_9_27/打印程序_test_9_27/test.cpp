4c74b58b62ba2273c5bf0bc0a54d27598a0dd6c4
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-10 23:39:27 +0800

    Add files via upload

---
+++ 打印程序_test_9_27/打印程序_test_9_27/test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+//int main()
+//{
+//	//std::cout << "hello" << ", " << "world" << std::endl;
+//	std::cout << "hello";
+//	std::cout << ", ";
+//	std::cout << "world";
+//	std::cout << std::endl;
+//
+//	return 0;
+//}
+////简单的打印函数，两种方式
+
+//int main()
+//{
+//	int val1, val2;
+//
+//	std::cout << "Enter two numbers:" << std::endl;
+//
+//	std::cin >> val1 >> val2;
+//
+//	std::cout << "The product of " << val1 << " and " << val2 <<
+//		" is " << val1 * val2 << std::endl;
+//
+//	return 0;
+//}
+//
+////输入数字的时候换不换行都可以
+
+//int main()
+//{
+//	int v1 = 3, v2 = 4;
+//
+//	std::cout << "The sum of " << v1;
+//	<< "and" << v2;//第一个 << 没有左侧运算对象
+//	<< " is " << v1 + v2 << std::endl;//同上
+//
+//	return 0;
+//}
+
