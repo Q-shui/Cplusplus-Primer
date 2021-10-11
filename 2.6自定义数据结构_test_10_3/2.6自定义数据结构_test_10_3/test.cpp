c96e37341792e6abdfaa850b00e842032b01efdc
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-11 22:46:08 +0800

    Add files via upload

---
+++ 2.6自定义数据结构_test_10_3/2.6自定义数据结构_test_10_3/test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+//int main()
+//{
+//	struct Sales_data
+//	{
+//		std::string bookNo;
+//		unsigned units_sold = 0;
+//		double revenue = 0.0;
+//	};
+//
+//
+//
+//	return 0;
+//}
+
+//struct Fro
+//{
+//
+//};
+//
+//int main()
+//{
+//
+//
+//	return 0;
+//}
+
+struct Sales_data
+{
+	std::string bookname;
+	int ID = 0;
+	double price = 0.0;
+};
+
+int main()
+{
+
+
+	return 0;
+}
