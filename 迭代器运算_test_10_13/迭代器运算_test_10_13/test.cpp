1a63d0b69e84069becbaf594a0080aca8e4123a6
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-16 22:46:08 +0800

    Add files via upload

---
+++ 迭代器运算_test_10_13/迭代器运算_test_10_13/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> scores(11, 0);
+	unsigned grade;
+
+	while (cin >> grade)
+	{
+		if (grade <= 100)
+			*(scores.begin() + grade / 10) += 1;
+ 	}
+
+	return 0;
+}
