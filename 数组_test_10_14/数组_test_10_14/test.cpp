8d919f5488bed23fc8c8770840f4c81410845e10
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-16 23:00:37 +0800

    Add files via upload

---
+++ 数组_test_10_14/数组_test_10_14/test.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+//初始化
+
+//int main()
+//{
+//	int a = 3;
+//	//int arr1[a];//错误，a是可修改的值
+//	
+//	const int b = 42;
+//	int arr[b];//数组长度必须是常量
+//
+//	int arr[] = { 10, 1 };//列表初始化
+//
+//	const unsigned sz = 3;
+//
+//	int ia1[sz] = { 1, 2, 3 };
+//
+//	int ia2[] = { 1, 2, 3 };
+//
+//	int ia3[5] = { 1, 2, 3 };//后面的值被初始化成0；
+//
+//	string is1[3] = { "hi", "hello" };//等价于 { "hi", "hello", "" };
+//
+//	//字符串字面值常量初始化字符数组
+//
+//	char a1[] = { 'c', '+', '+'};
+//
+//	char a2[] = { 'c', '+', '+', '\0' };
+//
+//	char a3[] = "c++";//自动含有'\0'
+//
+//	//不能用一个数组初始化另一个数组,也不能把一个数组赋值给另一个数组
+//	 
+//	return 0;
+//}
+
+string sa[10];
+
+int a1[10];
+
+int main()
+{
+	string sa2[10];
+
+	int ia2[10];
+
+	return 0;
+}
