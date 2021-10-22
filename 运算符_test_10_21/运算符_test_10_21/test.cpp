a7c1b4adfa1ced6b78b5dcb13e768c377052bdda
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-22 23:13:47 +0800

    Add files via upload

---
+++ 运算符_test_10_21/运算符_test_10_21/test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+//int main()
+//{
+//	int i = 2;
+//	int j = 3;
+//	int k = 4;
+//
+//	if (i < j < k)//i先和j比较得到一个布尔值，布尔值一般是0 或 1，如果k> 1则结果为真
+//		cout << 1 << endl;
+//
+//	int val;
+//
+//	if (val)//判断val的值是否为0;
+//		;
+//	if (!val)
+//		;
+//	if (val == true)//只有当val的值为1的时候才为真
+//		;
+//	//此过程中，true的类型会先被转换成int，即val == 1;
+//	int i;
+//
+//	while (cin >> i)
+//		if (i == 42)
+//			break;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b, c, d;
+//
+//	if (a > b && b > c && c > d)
+//		;
+//
+//	return 0;
+//}
