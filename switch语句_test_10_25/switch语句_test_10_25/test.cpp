399e25523c551ea8b8e107112e50af6d33689a62
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-25 22:11:26 +0800

    Add files via upload

---
+++ switch语句_test_10_25/switch语句_test_10_25/test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+//int main()
+//{
+//	int cnt = 0;
+//	char s;
+//	
+//	while (cin >> s)
+//		if (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u')
+//			cnt++;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int cnt = 0;
+//	char s;
+//
+//	while (cin >> s)
+//	{
+//		switch (s) {
+//		case 'a':
+//		case 'A':
+//		case 'e':
+//		case 'E':
+//		case 'i':
+//		case 'I':
+//		case 'o':
+//		case 'O':
+//		case 'u':
+//		case 'U':
+//			++cnt;
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
+
+int main()
+{
+
+
+	return 0;
+}
